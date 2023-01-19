64d792be7c745fe53f29df2ce68f467686b7a76e
Julien ROGER <julien.roger@net-helium.fr>
2023-01-19 17:34:20 +0100

    Migration Redmine 5.0.4

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), 'lib/redmine_documents_pops_hook_listener')
-require File.join(File.dirname(__FILE__), 'app/models/document')
-require File.join(File.dirname(__FILE__), 'app/models/attachment')
+require File.join(File.dirname(__FILE__), 'app/models/documents_pops')
+require File.join(File.dirname(__FILE__), 'app/models/attachments_pops')
 # require File.join(File.dirname(__FILE__), 'app/helpers/attachments_helper_patch')
 require File.join(File.dirname(__FILE__), 'app/controllers/documents_controller_patch')
 require File.join(File.dirname(__FILE__), 'app/controllers/attachments_controller_patch')
